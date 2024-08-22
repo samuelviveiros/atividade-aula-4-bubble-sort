eb581dd26fb2c6adf04d5f54c88d8fbcdb6928c3
Dartz <samuel.viveiros@gmail.com>
2024-08-21 21:47:09 -0300

    Update generate_input.py

--- generate_input.py
+++ generate_input.py
@@ -2,7 +2,6 @@ from random import randint
 
 MAX_SIZE = 100_000
 
-# Generate a text file with 1 billion of random integers
 print('Generating input file...')
 with open('input.txt', 'w') as integers:
     for i in range(1, MAX_SIZE + 1):
