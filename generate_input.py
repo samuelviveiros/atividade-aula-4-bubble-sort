3231ea0db307432f743989aa65bc1d420676da1e
Dartz <samuel.viveiros@gmail.com>
2024-08-28 18:24:21 -0300

    Update generate_input.py

--- generate_input.py
+++ generate_input.py
@@ -1,6 +1,7 @@
 from random import randint
 
-MAX_SIZE = 100_000
+#MAX_SIZE = 2600  # ~ 1 sec
+MAX_SIZE = 3750  # ~2 sec
 
 print('Generating input file...')
 with open('input.txt', 'w') as integers:
